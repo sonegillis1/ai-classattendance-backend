0860ee69f53c9db58761bc688fb6df2cc87074c1
Sone Gillis <blackhat@local>
2025-03-09 13:40:35 +0100

    updates dockerfile compose

--- classattendance/settings/production.py
+++ classattendance/settings/production.py
@@ -8,11 +8,11 @@ DEBUG = False
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST', default='localhost'),
-        'PORT': config('DB_PORT', default='5432'),
+        'NAME': config('DATABASE_NAME'),
+        'USER': config('DATABASE_USER'),
+        'PASSWORD': config('DATABASE_PASSWORD'),
+        'HOST': config('DATABASE_HOST', default='localhost'),
+        'PORT': config('DATABASE_HOST', default='5432'),
     }
 }
 
